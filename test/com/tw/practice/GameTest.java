cb73ce5d298a7635c93f08f83b29803683915d57
YKGDWADE3 <superwadeg3@gmail.com>
2018-01-15 15:29:53 +0800

    add:Game 单元测试

--- test/com/tw/practice/GameTest.java
+++ test/com/tw/practice/GameTest.java
@@ -4,16 +4,21 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class GameTest {
 
     @Mock
-    private NumberGenerator mockedGenerator;
+    private NumberGenerator mockedGenerator = Mockito.mock(NumberGenerator.class);
 
     @Mock
-    private Printer mockedPrinter;
+    private Printer mockedPrinter = Mockito.mock(Printer.class);
 
     @Mock
-    private NumberReader mockedReader;
+    private NumberReader mockedReader = Mockito.mock(NumberReader.class);
 
     @Test
     public void shouldReturn4A0BWhenInputSameNumber() throws Exception {
@@ -26,4 +31,16 @@ public class GameTest {
         Mockito.verify(mockedPrinter, Mockito.times(1)).print("please input a 4 digit number:");
         Mockito.verify(mockedPrinter, Mockito.times(1)).print("you win");
     }
+
+    @Test
+    public void shouldReturn0A4BWhenInputSameDigit() throws Exception {
+        Mockito.when(mockedGenerator.generate()).thenReturn("1234");
+        Mockito.when(mockedReader.read()) .thenReturn("4321");
+        Game game = new Game(mockedGenerator, mockedPrinter, mockedReader);
+        game.start();
+        Mockito.verify(mockedPrinter, Mockito.times(1)).print("please input a 4 digit number:");
+        Mockito.verify(mockedPrinter, Mockito.times(0)).print("you win");
+        Mockito.verify(mockedPrinter, Mockito.times(6)).print("0A4B");
+    }
+
 }
